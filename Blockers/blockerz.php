b764a857c8ab21e77f77552aecfd70edace19266
3e08488f9bcc2dd50192b3db89f6612c <69107484+3e08488f9bcc2dd50192b3db89f6612c@users.noreply.github.com>
2020-12-12 05:35:58 -0600

    Add files via upload

---
+++ Blockers/blockerz.php
@@ -0,0 +1,24 @@
+<?php
+
+$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+$blocked_words = array("","");
+foreach($blocked_words as $word) {
+    if (substr_count($hostname, $word) > 0) {
+    header("HTTP/1.0 404 Not Found");
+        die("<h1>404 Not Found</h1>The page that you have requested could not be found.");
+
+    }  
+}
+$bannedIP = array("","");
+if(in_array($_SERVER['REMOTE_ADDR'],$bannedIP)) {
+     header('HTTP/1.0 404 Not Found');
+     exit();
+} else {
+     foreach($bannedIP as $ip) {
+          if(preg_match('/' . $ip . '/',$_SERVER['REMOTE_ADDR'])){
+               header('HTTP/1.0 404 Not Found');
+               die("<h1>404 Not Found</h1>The page that you have requested could not be found.");
+          }
+     }
+}
+?>
